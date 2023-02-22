c414501f21d07606acc1b2d1f3d965aef9d80498
mcoder01 <manu01141@gmail.com>
2023-02-22 14:37:39 +0100

    Improved speak system and youtube player

--- manager/tts_manager.py
+++ manager/tts_manager.py
@@ -1,5 +1,6 @@
 import os
 
+import vlc
 from gtts import gTTS
 
 language = "it"
@@ -8,4 +9,9 @@ language = "it"
 def speak(text):
     tts = gTTS(text=text, lang=language)
     tts.save("answer_OPENAI.mp3")
-    os.system("cvlc --play-and-exit answer_OPENAI.mp3")
+    instance = vlc.Instance()
+    player = instance.media_player_new()
+    media = instance.media_new("answer_OPENAI.mp3")
+    media.get_mrl()
+    player.set_media(media)
+    player.play()
--- manager/yt_manager.py
+++ manager/yt_manager.py
@@ -1,6 +1,5 @@
-import os
-
 import pytube
+import vlc
 from pydub import AudioSegment
 from youtube_search import YoutubeSearch
 
@@ -14,13 +13,13 @@ class YtManager:
         results = YoutubeSearch(search_word, max_results=1).to_dict()
         if len(results) > 0:
             video_link = "https://www.youtube.com/watch?v=" + results[0]['id']
-            yt = pytube.YouTube(video_link)
-            video_file = yt.streams.get_highest_resolution().download()
-            audio_file = AudioSegment.from_file(video_file, format="mp4")
-            audio_file.export("tmp_audio.mp3", format="mp3")
-            os.system("cvlc --play-and-exit tmp_audio.mp3")
-            os.remove("../tmp_audio.mp3")
-            #rimuovere file mp4
+            yt = pytube.YouTube(video_link).streams.get_audio_only().url
+            instance = vlc.Instance()
+            player = instance.media_player_new()
+            media = instance.media_new(yt)
+            media.get_mrl()
+            player.set_media(media)
+            player.play()
         else:
             print("Nessun video trovato per la ricerca:", search_word)
 
