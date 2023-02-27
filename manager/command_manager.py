4b4fe44322fb9ef5175e5703ad18cf6d8f65d741
alessiovetrano <alessio.vetrano98@gmail.com>
2023-02-27 20:07:49 +0100

    add function manager_audio(not work) add playlist path

--- manager/command_manager.py
+++ manager/command_manager.py
@@ -5,7 +5,6 @@ from manager.tts_manager import speak
 from manager.weather_manager import WeatherManager
 from manager.yt_manager import YtManager
 
-
 class CommandManager:
     def __init__(self):
         self.yt_manager = YtManager()
@@ -16,7 +15,8 @@ class CommandManager:
             "download_yt_cmds": [r"download (.*)", r"puoi scaricare (.*)"],
             "emilio_cmds": [r"mortimer", r"bellibus"],
             "weather_cmds": [r"puoi dirmi il meteo di (.*)", r"puoi dirmi il meteo a (.*)", r"dimmi il meteo a (.*)",
-                             r"puoi dirmi il meteo attuale di (.*)", r"meteo  di (.*)", r"meteo (.*)", r"meteo a (.*)"]
+                             r"puoi dirmi il meteo attuale di (.*)", r"meteo  di (.*)", r"meteo (.*)", r"meteo a (.*)"],
+            "manager_audio": [r"stop", r"stoppa",r"rimetti in play"]
         }
 
     def manage(self, keyword):
@@ -24,17 +24,21 @@ class CommandManager:
             for command in self.cmds[group]:
                 match = re.search(command, keyword)
                 if match:
-                    arg = match.group(1)
-                    print("Pattern trovato!")
+                    if match.group(1):
+                        arg = match.group(1)
+                    else:
+                        arg = match.string()
+                    print(arg)
                     if group == "play_yt_cmds":
                         self.yt_manager.play_yt(arg)
                     elif group == "download_yt_cmds":
                         self.yt_manager.download_yt(arg)
                     elif group == "emilio_cmds":
                         speak(arg)
+                    elif group == "manager_audio":
+                        self.yt_manager.manager_audio(arg)
                     elif group == "weather_cmds":
                         self.weather_manager.ask_weather(arg)
                     return
-
         print("Pattern non trovato!")
         self.gpt_manager.ask(keyword)
--- manager/yt_manager.py
+++ manager/yt_manager.py
@@ -1,35 +1,53 @@
 import pytube
 import vlc
-
+import os
 from youtube_search import YoutubeSearch
 from manager.tts_manager import speak
 
 
 class YtManager:
+    def __init__(self):
+        self.instance = vlc.Instance()
+        self.player = self.instance.media_player_new()
+
     def play_yt(self, keyword):
-        speak("Certo, riproduco: " + keyword)
-        search_word = keyword
-        results = YoutubeSearch(search_word, max_results=1).to_dict()
-        if len(results) > 0:
-            video_link = "https://www.youtube.com/watch?v=" + results[0]['id']
-            yt = pytube.YouTube(video_link).streams.get_audio_only().url
-            instance = vlc.Instance()
-            player = instance.media_player_new()
-            media = instance.media_new(yt)
-            media.get_mrl()
-            player.set_media(media)
-            player.play()
+        if "playlist" in keyword:
+            if not (os.path.exists("playlist")):
+                speak("Non esiste alcuna playlist da riprodurre")
+            else:
+                speak("Certo, riproduco la tua playlist")
+                for filename in os.listdir("playlist"):
+                    audio_track = os.path.join("playlist", filename)
         else:
-            print("Nessun video trovato per la ricerca:", search_word)
+            speak("Certo, riproduco " + keyword)
+            search_word = keyword
+            results = YoutubeSearch(search_word, max_results=1).to_dict()
+            if len(results) > 0:
+                video_link = "https://www.youtube.com/watch?v=" + results[0]['id']
+                audio_track = pytube.YouTube(video_link).streams.get_audio_only().url
+            else:
+                speak("Nessun video trovato per  ", search_word)
+        media = self.instance.media_new(audio_track)
+        media.get_mrl()
+        self.player.set_media(media)
+        self.player.play()
 
     def download_yt(self, keyword):
-        speak("Certo, scarico: " + keyword)
-        search_word = keyword
-        results = YoutubeSearch(search_word, max_results=1).to_dict()
-        if len(results) > 0:
-            #AGGIUNGERE PATH CARTELLA PER PLAYLIST
-            video_link = "https://www.youtube.com/watch?v=" + results[0]['id']
-            video = pytube.YouTube(video_link)
-            video.streams.get_audio_only().download(filename=video.title+".mp3")
+        if not (os.path.exists("playlist")):
+            os.mkdir("playlist")
         else:
-            print("Nessun video trovato per la ricerca:", search_word)
+            speak("Certo, scarico: " + keyword)
+            search_word = keyword
+            results = YoutubeSearch(search_word, max_results=1).to_dict()
+            if len(results) > 0:
+                video_link = "https://www.youtube.com/watch?v=" + results[0]['id']
+                video = pytube.YouTube(video_link)
+                video.streams.get_audio_only().download(output_path="playlist", filename=video.title + ".mp3")
+            else:
+                speak("Nessun video trovato per  ", search_word)
+
+    def manager_audio(self, keyword):
+        if "pausa" or "stoppa" or "stop" in keyword:
+            self.player.pause()
+        elif "play" or "rimetti" in keyword:
+            self.player.play()
