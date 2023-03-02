2edf37df077fd6ac8b2ad703fc66530fe47d4ae4
alessiovetrano <alessio.vetrano98@gmail.com>
2023-03-02 01:25:20 +0100

    add weather also for tomorrow

--- manager/weather_manager.py
+++ manager/weather_manager.py
@@ -7,15 +7,26 @@ weather_api = "a26a13556f93a14df0a58e1fd6328495"
 
 class WeatherManager:
     def ask_weather(self, keyword):
-        url = f"http://api.weatherstack.com/current?access_key={weather_api}&query={keyword}"
+        if "domani" in keyword.lower():
+            print("Tomorrow")
+            keyword.replace('domani a ', '').strip()
+            url = f"http://api.weatherstack.com/forecast?access_key={weather_api}&query={keyword}"
+        else:
+            print("Today")
+            url = f"http://api.weatherstack.com/current?access_key={weather_api}&query={keyword}"
+
         response = requests.get(url)
         data = response.json()
         if 'current' in data:
             temperature = data['current']['temperature']
             weather_description = data['current']['weather_descriptions'][0]
-            # description_translated = translation(weather_description)
-            # BUG poiche weather_description è in inglese, non trovo API gratuite
-            speak(f"Temperatura attuale a {keyword}: {temperature} gradi Celsius con le seguenti condizioni: "
-                  + weather_description)
+            if "domani" in keyword.lower():
+                keyword.replace('domani a ', '').strip()
+                speak(f"Temperatura a {keyword} per domani: {temperature} gradi Celsius con le seguenti condizioni: "
+                      + weather_description)
+            else:
+                speak(f"Temperatura attuale a {keyword}: {temperature} gradi Celsius con le seguenti condizioni: "
+                      + weather_description)
         else:
             speak(f"Impossibile ottenere le informazioni meteorologiche per {keyword}")
+
